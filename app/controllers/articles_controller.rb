1d66036c387fb3e22a220592a0c9656dd0d77543
Mathieu Lague <mathieu.lague@laposte.net>
2017-09-06 09:58:47 +0200

    add simple_form_for articles#new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = current_user
     if @article.save
       redirect_to articles_path
     else
