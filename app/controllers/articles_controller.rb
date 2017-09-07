7e9e680629fcb9750bc7a10b0bc28d830a7096b4
Mathieu Lague <mathieu.lague@laposte.net>
2017-09-07 11:38:15 +0200

    display actions buttons by role

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :destroy]
 
   def index
-    @articles = Article.all
+    @articles = policy_scope(Article)
   end
 
   def new
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+
+  def button_new_article
+    if policy(Article).new?
+      link_to "Write your publication", new_article_path(@article), class: "btn btn-primary"
+    end
+  end
+
+  def button_edit_article(article)
+    if policy(article).update?
+      link_to edit_article_path(article) do
+        '<i class="fa fa-pencil"></i>'.html_safe
+      end
+    end
+  end
+
+  def button_destroy_article(article)
+    if policy(article).destroy?
+      link_to article_path(article) do
+        '<i class="fa fa-trash"></i>'.html_safe
+      end
+    end
+  end
 end
