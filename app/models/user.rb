3724b49b52a2ada94e3c0937cec200e0720ef3e4
Konstantin BIFERT <konstantin.bifert@gmail.com>
2017-09-07 15:48:51 +0200

    almost working interface

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable,
   # devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :upvotes, :as => :upvotable
-  has_many :user_tags
+  has_many :upvotes, :as => :upvotable, dependent: :destroy
+  has_many :upvotes, dependent: :destroy
+  has_many :user_tags, dependent: :destroy
   has_many :tags, through: :user_tags
   has_many :article_tags, through: :tags
   has_many :articles
