eacaf74b8ad3d5cc5ec6a3cacba93db6a5ac1c45
Mathieu Lague <mathieu.lague@laposte.net>
2017-09-07 12:00:52 +0200

    translate flsh alert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   # Uncomment when you *really understand* Pundit!
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   def user_not_authorized
-    flash[:alert] = "You are not authorized to perform this action."
+    flash[:alert] = "Vous n'êtes pas authorisé à effectuer cette action."
     redirect_to authenticated_root_path
   end
 
