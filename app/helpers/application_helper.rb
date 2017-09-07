3fd98ec0b0e2ae9040b0cf4196f167cf8d14f91c
Mathieu Lague <mathieu.lague@laposte.net>
2017-09-07 18:20:54 +0200

    add card html & css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,25 +2,25 @@ module ApplicationHelper
 
   def button_new_article
     if policy(Article).new?
-      link_to new_article_path(@article) do
-        '<i class="fa fa-plus green-lg" aria-hidden="true"></i>'.html_safe
-      end
+      link_to '<i class="fa fa-plus green-lg" aria-hidden="true"></i>'.html_safe, new_article_path(@article)
     end
   end
 
   def button_edit_article(article)
     if policy(article).update?
-      link_to edit_article_path(article) do
-        '<i class="fa fa-pencil"></i>'.html_safe
-      end
+      link_to '<i class="fa fa-pencil"></i>'.html_safe, edit_article_path(article)
     end
   end
 
   def button_destroy_article(article)
     if policy(article).destroy?
-      link_to article_path(article) do
-        '<i class="fa fa-trash"></i>'.html_safe
-      end
+      link_to '<i class="fa fa-trash"></i>'.html_safe, article_path(article)
+    end
+  end
+
+  def button_external_link(article)
+    link_to article.url, target: "_blank" do
+      '<i class="fa fa-external-link"></i>'.html_safe
     end
   end
 
