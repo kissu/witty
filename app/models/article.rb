35be24bfe4a7fd25ad8cf637b6d903ff5947822b
fouadpy <perceval@protonmail.com>
2017-09-08 14:25:31 +0200

    Revert "Merge pull request #12 from kissu/upvote-articles"
    This reverts commit 7915efe81a666375cd970cb6eba4527fcfb864c8, reversing
    changes made to 2c359fc6e322157087a9b60e6d27d98a9d686420.

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,4 @@ class Article < ApplicationRecord
   has_many :upvotes, :as => :upvotable
   has_many :article_tags, dependent: :destroy
   has_many :tags, through: :article_tags
-
-  include UpvotableConcern
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable,
   # devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  has_many :user_tags
+  has_many :upvotes, :as => :upvotable, dependent: :destroy
+  has_many :upvotes, dependent: :destroy
+  has_many :user_tags, dependent: :destroy
   has_many :tags, through: :user_tags
   has_many :article_tags, through: :tags
   has_many :articles
 
   enum role: { entrepreneur: 0, expert: 10, investisseur: 20, admin: 30 }
-
-  include UpvotableConcern
 end
