774b1daa17e70dfc4abe6ebfc526fc2f88c8a73a
fouadpy <perceval@protonmail.com>
2017-09-08 14:25:31 +0200

    Revert "Merge pull request #12 from kissu/upvote-articles"
    This reverts commit 7915efe81a666375cd970cb6eba4527fcfb864c8, reversing
    changes made to 2c359fc6e322157087a9b60e6d27d98a9d686420.

--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -1,5 +1,4 @@
 class Upvote < ApplicationRecord
   belongs_to :upvotable, :polymorphic => true
   belongs_to :user
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,5 @@ Rails.application.routes.draw do
   end
 
   resources :articles
-  resources :upvotes, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
