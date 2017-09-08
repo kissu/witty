fd9482264bb7c77c341ca595da80f732a32e7130
Konstantin BIFERT <konstantin.bifert@gmail.com>
2017-09-08 17:22:13 +0200

    almost functionnal order

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,33 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = policy_scope(Article)
+
+    # order_articles = []
+    # @articles.each do |article|
+    #   order_articles << article
+    # end
+    sorted_indexed_array_most_upvoted_first = []
+    @articles = Article.all
+
+    # @articles.each_with_index do |article, index|
+    #   sorted_indexed_array_most_upvoted_first[0] = index
+    #   sorted_indexed_array_most_upvoted_first[1] = article.upvotes.size
+    # end
+    # p sorted_array_most_upvoted_first.sort.reverse
+
+
+    # arr = [[0, Article.first.upvotes.size], [1, Article.second.upvotes.size]]
+    arr = []
+    @articles.each_with_index do |article, index|
+      arr << [ article.id, article.upvotes.size ]
+    end
+    arr.sort_by(&:last)
+    byebug
+    arr.each_index do |x|
+      puts "Article ##{arr[x][0]} has #{arr[x][1]} upvotes"
+    end
+
+    # @articles.sort { |a,b| a.created_at <=> b.created_at }
   end
 
   def new
