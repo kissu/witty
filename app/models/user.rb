32cd3dfd5f63f6dda4b6e2fadf5e1612a121873c
Mathieu Lague <mathieu.lague@laposte.net>
2017-09-11 10:19:20 +0200

    rajout des dependence de destroy sur les votes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :upvotes, :as => :upvotable, dependent: :destroy
-  has_many :votes, class_name: "Upvote", foreign_key: "user_id"
+  has_many :votes, class_name: "Upvote", foreign_key: "user_id", dependent: :destroy
   has_many :user_tags, dependent: :destroy
   has_many :tags, through: :user_tags
   has_many :article_tags, through: :tags
