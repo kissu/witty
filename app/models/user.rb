a0ab71c9f6d04a75ac054b9a13c42664d3672a96
Mathieu Lague <mathieu.lague@laposte.net>
2017-09-11 09:47:03 +0200

    remplacement dans User du has_many upvotes par has_many :notes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :upvotes, :as => :upvotable, dependent: :destroy
-  has_many :upvotes, dependent: :destroy
+  has_many :notes, class_name: "Upvote", foreign_key: "user_id"
   has_many :user_tags, dependent: :destroy
   has_many :tags, through: :user_tags
   has_many :article_tags, through: :tags
