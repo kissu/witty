e76a35ab463d94534453d11b247d3f617178ddb3
Konstantin BIFERT <konstantin.bifert@gmail.com>
2017-09-11 13:47:42 +0200

    first wave

--- db/seeds.rb
+++ db/seeds.rb
@@ -57,9 +57,9 @@ puts "Tags for articles created"
 ArticleTag.create!(article: Article.first, tag: Tag.first)
 ArticleTag.create!(article: Article.second, tag: Tag.second)
 ArticleTag.create!(article: Article.second, tag: Tag.third)
-ArticleTag.create!(article: Article.third, tag: Tag.first)
-ArticleTag.create!(article: Article.third, tag: Tag.second)
 ArticleTag.create!(article: Article.third, tag: Tag.third)
+ArticleTag.create!(article: Article.third, tag: Tag.second)
+# ArticleTag.create!(article: Article.third, tag: Tag.third)
 ArticleTag.create!(article: Article.fourth, tag: Tag.fourth)
 
 # Link users & tags
