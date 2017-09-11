4059e231b6a9d2b5f18e730714beec34b17e2845
Mathieu Lague <mathieu.lague@laposte.net>
2017-09-11 10:27:11 +0200

    add : nullify sur user has_many articles. Ne detruit pas les articles si le user est supprimé

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :user_tags, dependent: :destroy
   has_many :tags, through: :user_tags
   has_many :article_tags, through: :tags
-  has_many :articles, dependent: :destroy
+  has_many :articles, dependent: :nullify
 
   enum role: { entrepreneur: 0, expert: 10, investisseur: 20, super_admin: 30}
 
