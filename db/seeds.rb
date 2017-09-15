766ae2262f0b26bfbd7cbbe9fa8eec67d4dcac32
Mathieu Lague <mathieu.lague@laposte.net>
2017-09-15 12:26:22 +0200

    modif seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,19 @@
 puts "Destroying all previous generated models"
 Upvote.destroy_all
-ArticleTag.destroy_all
-UserTag.destroy_all
 Tag.destroy_all
 Article.destroy_all
 User.destroy_all
+Contact.destroy_all
 
 # User Generation
 users_array = []
-users_array << User.create!(title: "Financeur de startup",description: "à contacter pour toutes vos demande de financement",email: "florent@aquiti.fr", first_name: "florent".capitalize, invitation_accepted_at: DateTime.now, last_name: "merian".capitalize, password: "aquiti", company: "Aquiti", role: "super_admin")
-users_array << User.create!(title: "Entrepreneur dans la santé",description: "serial entrepreneur, a pour ambition d'apporter des solutions ",email: "fouad@aquiti.fr", password: "aquiti", role: "super_admin", invitation_accepted_at: DateTime.now)
-users_array << User.create!(title: "Expert de GIT mais pas de Github",description: "n'appelez pas si vous avez besoin de déployer sur heroku",email: "kos@aquiti.fr", password: "aquiti", role: "super_admin", invitation_accepted_at: DateTime.now)
-users_array << User.create!(title: "Entrepreneur",description: "Le meilleur of course",email: "mathieu@aquiti.fr", password: "aquiti", role: "super_admin", invitation_accepted_at: DateTime.now)
-users_array << User.create!(title: "Entrepreneur de la silicon vallée",description: "Expérience dans beaucoup de domaine",email: "en@test", password: "test", role: "entrepreneur", invitation_accepted_at: DateTime.now)
-users_array << User.create!(title: "Investisseur, buisiness Angel",description: "$$$$$$",email: "in@test", password: "test", role: "investisseur", invitation_accepted_at: DateTime.now)
-users_array << User.create!(title: "Expert en mécanique quantique",description: "vous comprendrez rien de toute façon",email: "ex@test", password: "test", role: "expert", invitation_accepted_at: DateTime.now)
+users_array << User.create!(email: "florent@aquiti.fr",password: "aquiti", role: "super_admin", invitation_accepted_at: DateTime.now)
+users_array << User.create!(email: "fouad@aquiti.fr", password: "aquiti", role: "super_admin", invitation_accepted_at: DateTime.now)
+users_array << User.create!(email: "kos@aquiti.fr", password: "aquiti", role: "super_admin", invitation_accepted_at: DateTime.now)
+users_array << User.create!(email: "mathieu@aquiti.fr", password: "aquiti", role: "super_admin", invitation_accepted_at: DateTime.now)
+users_array << User.create!(email: "en@test", password: "test", role: "entrepreneur", invitation_accepted_at: DateTime.now)
+users_array << User.create!(email: "in@test", password: "test", role: "investisseur", invitation_accepted_at: DateTime.now)
+users_array << User.create!(email: "ex@test", password: "test", role: "expert", invitation_accepted_at: DateTime.now)
 
 puts "Users created"
 
@@ -36,19 +35,24 @@ puts "Tags for articles created"
 
 # Link articles & tags
 articles_array.each do |article|
-  ArticleTag.create!(article: article, tag: tags_array.sample)
+  article.tags << tags_array.sample
 end
 
-# Link users & tags
-rand(8..12).times do
-  UserTag.create!(tag: tags_array.sample, user: users_array.sample)
-end
-
-puts "Tags linked to articles and users"
+puts "Tags linked to articles"
 
 # Upvote Generation
 rand(50..60).times do
-  Upvote.create!(upvotable: articles_array.sample, user: users_array.sample)
+  Upvote.create!(article: articles_array.sample, user: users_array.sample)
 end
 
 puts "Upvotes done"
+
+#Contacts generation
+10.times do
+  c = Contact.create(title: Faker::Job.title, description: Faker::Lorem.sentence(3))
+  2.times do
+  c.tags << tags_array.sample
+  end
+end
+
+puts "Contact done"
