ebb3acc88a023ccdee67e501d2c727e5d1d85085
Mathieu Lague <mathieu.lague@laposte.net>
2017-09-15 10:16:43 +0200

    schema db mis à jour

--- db/migrate/20170915065525_add_title_and_descrition_to_contacts.rb
+++ db/migrate/20170915065525_add_title_and_descrition_to_contacts.rb
@@ -1,6 +1,6 @@
 class AddTitleAndDescritionToContacts < ActiveRecord::Migration[5.1]
   def change
     add_column :contacts, :title, :string
-    add_column :contacts, :description, :string
+    add_column :contacts, :description, :string, limit: 140
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170914155602) do
+ActiveRecord::Schema.define(version: 20170915081502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 20170914155602) do
   create_table "contacts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "description", limit: 140
   end
 
   create_table "contacts_tags", id: false, force: :cascade do |t|
@@ -83,14 +85,9 @@ ActiveRecord::Schema.define(version: 20170914155602) do
     t.datetime "last_sign_in_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.string "company"
     t.integer "role", default: 0
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.string "title"
-    t.string "description"
     t.string "invitation_token"
     t.datetime "invitation_created_at"
     t.datetime "invitation_sent_at"
