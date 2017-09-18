9e34a186452ef617b3bdf23c63be5bb9899a4db9
Konstantin BIFERT <konstantin.bifert@gmail.com>
2017-09-18 06:05:14 +0200

    need to check some tricky stuff, refacto the code, make the js functionnal (divs still splitted in 2 pls) and some more stuff I guess...

--- app/assets/javascripts/custom_filterizr.js
+++ app/assets/javascripts/custom_filterizr.js
@@ -1,4 +1,18 @@
-// $(document).ready(function(){
-//   var filterizd = $('.filtr-container').filterizr({
-//   });
-// });
+$(document).ready(function(){
+  // var filterizd = $('.filtr-contact-container').filterizr({
+  //    animationDuration: 0.5,
+  //    callbacks: {
+  //       onFilteringStart: function() { },
+  //       onFilteringEnd: function() { },
+  //       onShufflingStart: function() { },
+  //       onShufflingEnd: function() { },
+  //       onSortingStart: function() { },
+  //       onSortingEnd: function() { }
+  //    },
+  //    delay: 0,
+  //    delayMode: 'progressive',
+  //    layout: 'packed',
+  //    selector: '.filtr-contact-container',
+  //    setupControls: true
+  // });
+});
