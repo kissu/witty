c0b3eaf5fef1bd9772fcff3a1505170269563b65
Konstantin BIFERT <konstantin.bifert@gmail.com>
2017-09-20 00:57:18 +0200

    button I understand implemented wo/ bootstrap so

--- app/assets/javascripts/custom_bootstrap.js
+++ app/assets/javascripts/custom_bootstrap.js
@@ -1,14 +1,5 @@
 $(document).ready(function(){
-  $('#myTabs a').click(function (e) {
-    e.preventDefault()
-    $(this).tab('show');
-    // if (this) {
-    //   console.log("hello");
-    // }
-  })
-
-  // $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-  //   console.log(e);
-  //   e.relatedTarget // previous active tab
-  // })
+  $('a[href*="text"]').last().click(function(){
+    $('.understood-button-block .btn').removeClass('hidden');
+  });
 });
