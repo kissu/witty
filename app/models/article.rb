69d6ecb9b19f079c28c3e662602b84bd8ad45534
Mathieu Lague <mathieu.lague@laposte.net>
2017-09-21 13:32:18 +0200

    maj validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,9 @@ class Article < ApplicationRecord
   has_many :upvotes, dependent: :destroy
   has_and_belongs_to_many :tags
 
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :url, presence: true
+
   include UpvotableConcern
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,6 @@
 class Contact < ApplicationRecord
   has_and_belongs_to_many :tags
+
   validates :description, presence: true
   validates :title, presence: true
 end
