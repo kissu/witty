8185bb87b6b43c042a7febe8bbd18cde39a21ffc
Konstantin BIFERT <konstantin.bifert@gmail.com>
2017-09-21 03:44:34 +0200

    structure (html & css) refacto, align nazi, css more precise on it's classes, filters working on both sections (knowledge & network)

--- app/assets/javascripts/custom_isotope.js
+++ app/assets/javascripts/custom_isotope.js
@@ -1,22 +1,25 @@
 $(document).ready(function(){
   // https://codepen.io/kissu/pen/dVMzvb
-  var $container = $('.contact-cards-container').isotope({
-    itemSelector: '.filtr-item'
+  var $knowledge_container = $('.article-upvoted-cards-container').isotope({
+    itemSelector: '.filtered-item'
+  });
+  var $network_container = $('.contact-cards-container').isotope({
+    itemSelector: '.filtered-item'
   });
 
   var $checkboxes = $('.button-tags-container .checkbox input');
   $checkboxes.change(function(){
     var filters = [];
-    // get checked checkboxes values
     $checkboxes.filter(':checked').each(function(){
       filters.push( this.value );
     });
-    // ['.red', '.blue'] -> '.red, .blue'
+
     filters = filters.join(', ');
-    $container.isotope({ filter: filters });
+    $knowledge_container.isotope({ filter: filters });
+    $network_container.isotope({ filter: filters });
   });
 
   $('#shuffle').click(function(){
-    $container.isotope('shuffle');
+    $network_container.isotope('shuffle');
   });
 })
