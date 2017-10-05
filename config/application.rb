5b8a229071e6c30f331e7e50b39d02d242db5f88
Konstantin BIFERT <konstantin.bifert@gmail.com>
2017-10-05 16:44:04 +0200

    cancel last renaming added

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Witty
+module Aquiti
   class Application < Rails::Application
     config.generators do |generate|
       generate.assets false
