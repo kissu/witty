44f33d9ca6c3501ff06ace43b4eaacb8a1a2d198
Konstantin BIFERT <konstantin.bifert@gmail.com>
2017-10-05 16:37:48 +0200

    css changes essentially

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Aquiti
+module Witty
   class Application < Rails::Application
     config.generators do |generate|
       generate.assets false
